a64d3d119d7019841b5bb577ee61b4c416bf75c5
Kenny Le <kennyle1412@gmail.com>
2024-03-05 22:21:59 -0600

    feat: remove passmanager and just use .env file

--- craftyllamabot/main.py
+++ craftyllamabot/main.py
@@ -1,12 +1,11 @@
 import discord
 
 from .logger_config import logger
-from .passmanager import PassManager
+from .settings import settings
 
-pm: PassManager = PassManager()
-client_key: str | None = pm.get_password("discord/token")
-if not client_key:
-    raise ValueError("Discord token not found in passmanager.")
+bot_token: str = settings.bot_token.get_secret_value()
+if not bot_token:
+    raise ValueError("Discord token not found in settings.")
 
 intents: discord.flags.Intents = discord.Intents.default()
 intents.message_content = True
@@ -40,4 +39,4 @@ async def on_message(message: discord.Message) -> None:
         logger.info("Message contains a link.")
 
 
-client.run(client_key)
+client.run(bot_token)
--- craftyllamabot/settings.py
+++ craftyllamabot/settings.py
@@ -1,10 +1,16 @@
-from pydantic import Field
+from pydantic import SecretStr
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    api_key: str = Field()
+    bot_token: SecretStr
 
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()  # type: ignore
 
 if __name__ == "__main__":
-    print(Settings().model_dump())
+    print(Settings().model_dump())  # type: ignore
