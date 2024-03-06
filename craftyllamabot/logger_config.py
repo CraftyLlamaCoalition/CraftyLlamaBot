e5b76f22466a75594dec1b7b90cafc8547ffc57a
Kenny Le <kennyle1412@gmail.com>
2024-03-05 22:13:22 -0600

    feat: create logs folder in cwd

--- craftyllamabot/logger_config.py
+++ craftyllamabot/logger_config.py
@@ -1,5 +1,16 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
+
+LOG_DIR: str = "./logs"
+LOG_FILE: str = os.path.join(LOG_DIR, "craftyllamabot.log")
+
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+if not os.path.exists(LOG_FILE):
+    with open(LOG_FILE, "w") as f:
+        f.write("")
 
 # Create a logger
 logger = logging.getLogger("CraftyLlamaBot")
@@ -8,7 +19,7 @@ logger.setLevel(logging.DEBUG)
 # Create handlers for console and file
 console_handler = logging.StreamHandler()
 file_handler = RotatingFileHandler(
-    "craftyllamalogfile.log",
+    LOG_FILE,
     maxBytes=1024 * 1024 * 5,
     backupCount=5,
 )
